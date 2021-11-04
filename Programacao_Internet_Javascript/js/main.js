310c5dff623089bf207bf93593edc2662426b87e
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2021-11-04 20:37:44 -0300

    Meu primeiro commit

---
+++ Programacao_Internet_Javascript/js/main.js
@@ -0,0 +1,40 @@
+/*var nome = "Jane Fernanda";
+var idade = 39;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+//alert(nome + " tem " + idade + " anos");
+
+alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+
+alert(frase.replace("Japão", "Brasil"));
+
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+lista.pop();
+alert(lista);
+alert(lista[1]);
+alert(lista.length);
+alert(lista.reverse());
+alert(lista);
+alert(lista.toString());
+alert(lista.toString()[0]);
+
+console.log(lista.join(" - "));
+
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+console.log(frutas);
