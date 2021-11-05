3e39f4ab6ba04d7c3af8490058119e08e11c4d4c
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2021-11-05 15:47:47 -0300

    Testes Javascript

--- Programacao_Internet_Javascript/js/main.js
+++ Programacao_Internet_Javascript/js/main.js
@@ -39,10 +39,12 @@ alert(frutas[1].nome);
 
 console.log(frutas);*/
 
-var idade = 18;
+/*var idade = prompt("Qual sua idade");
 
-if(idade > 18) {
+if(idade >= 18) {
     alert("Maior de idade");
 } else {
     alert("Menor de idade");
-};
+}; */
+
+var count = 5;
