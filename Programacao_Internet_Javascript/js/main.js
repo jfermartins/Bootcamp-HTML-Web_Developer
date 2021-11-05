dd4ef7743d4f1e9d8958608f2aac02de820ae6d4
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2021-11-05 17:11:52 -0300

    Uso de funções

--- Programacao_Internet_Javascript/js/main.js
+++ Programacao_Internet_Javascript/js/main.js
@@ -71,3 +71,14 @@ alert (d.getDay());
 alert (d.getHour());
 */
 
+function soma (n1, n2) {
+    return n1 + n2;
+}
+
+function setReplace (frase, nome, novo_nome) {
+    return frase.replace (nome, novo_nome)
+}
+
+alert (soma(5, 10));
+alert (setReplace("Vai Japão", "Japão", "Brasil"));
+
