38ebb1a50210204fe072429247e8de6742463e1d
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2021-11-09 21:27:29 -0300

    Functions javascript

--- Programacao_Internet_Javascript/js/main.js
+++ Programacao_Internet_Javascript/js/main.js
@@ -71,7 +71,7 @@ alert (d.getDay());
 alert (d.getHour());
 */
 
-function soma (n1, n2) {
+/*function soma (n1, n2) {
     return n1 + n2;
 }
 
@@ -81,4 +81,18 @@ function setReplace (frase, nome, novo_nome) {
 
 alert (soma(5, 10));
 alert (setReplace("Vai Japão", "Japão", "Brasil"));
+*/
 
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+
+
+var idade = prompt ("Qual a sua idade");
+console.log(validaIdade(idade));
