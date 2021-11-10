b8c1b038711fbe4a081ff4517426ce0f6a0ce4e9
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2021-11-09 23:11:53 -0300

    Criação pasta desafios

---
+++ Desafios/JS/Folha-pagamento.js
@@ -0,0 +1,9 @@
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+
+// Escreva o seu código aqui
+let total = parseFloat(valor2 * valor3).toFixed(2);
+
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + total);
